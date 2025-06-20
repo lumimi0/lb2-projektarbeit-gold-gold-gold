effc5ed9f43bde2bbd9493e4fe56546d263774ef
lumimi0 <luca.minoti@gmail.com>
2025-06-20 20:12:26 +0200

    Delete backend directory

--- backend/src/main/java/com/bbzbl/M347/repository/CaseRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bbzbl.M347.repository;
-
-import com.bbzbl.M347.model.Case;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CaseRepository extends JpaRepository<Case, Long> {
-}
