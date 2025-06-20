5e332696d00e236f59d83bbdf0f2a60812088095
lumimi0 <luca.minoti@gmail.com>
2025-06-20 17:29:17 +0200

    Delete frontend directory

--- frontend/src/app/models/skin.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Skin {
-  id: number;
-  name: string;
-  rarity: 'Consumer' | 'Industrial' | 'Mil-Spec' | 'Restricted' | 'Classified' | 'Covert' | 'Exceedingly Rare';
-  price: number;
-  imageUrl: string;
-  caseId: number;
-}
